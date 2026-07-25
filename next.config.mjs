5a693dcd86dcef1e272fc2ddb0c2f50c910c48bb
Sneha koshta <snehakoshta1@gmail.com>
2026-07-25 21:28:14 +0530

    fix: simplify next.config to fix 502

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,8 @@
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.join(__dirname, "src"),
-    };
-    return config;
   },
   headers: async () => [
     {
